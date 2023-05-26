c36088d7e5cec6841e3ffc125d51f357445b313b
Leyla Callahan <leyla.callahan@yahoo.com>
2023-05-25 20:29:02 -0400

    added env variables

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize("tech_blog", "root", "Dtrus2022!!!!", {
-    host: "localhost",
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: process.env.DB_HOST,
     dialect: "mysql",
     port: 3306,
   });
